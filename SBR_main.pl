f03cf274d40c7528d858e0eaa029a3583893bced
fabou <fabian_amman@gmx.net>
2009-06-17 11:22:29 +0200

    main und subroutinen vorschlaege

--- SBR_main.pl
+++ SBR_main.pl
@@ -1,34 +1,54 @@
 #!usr/local/bin/perl -w
 
-use strict;
+#use strict;
 use warnings;
 use Data::Dumper;
 use Getopt::Long;
 use Pod::Usage;
 use vars qw/@RaceTrack %STATE $L $map_file/;
 
-$L = 0;
-my $c =0; # for testing purpose
+$L = 0;                          # wenn jemand das ziel erreich wird $L=1
+my $c =0;                        # for testing purpose
+my @modes = qw/ car1 car2/;      # liste aller heuristik subroutinen
 
 pod2usage(-verbose => 0)
     unless GetOptions(
-      "racer:s" => sub { %STATE=split(':', $_[1]) },
-      "track:s" => sub { $map_file = $_[1] },
+      "racer:s" => sub { my @racers =split(",", $_[1]);
+			 foreach my $l (@racers) {
+			   my @racer = split (":", $l);
+			   $STATE{$racer[0]}->{'mode'}="$racer[1]";
+			 }
+                       },
+      "modes"   => sub{print "@modes\n";
+		       exit;
+		       },
+      "track:s" => sub{ $map_file = $_[1] },
       "help|?"  => sub{pod2usage(-verbose => 1)},
       "man"     => sub{pod2usage(-verbose => 2)}
     );
 
 
 #liest extern generierte Strecke nach dem Muster von racetrack.txt ein
-#start felder sind alle felder in der untersten zeile die mit 1 markiert sind
-#ziel felder sind alle felder der obersten zeile die mit 1 markiert sind
+
 @RaceTrack = readin_textfile();
 
-while ($L == 0) {
+&set_start()
 
- $STATE{car} = car(%STATE);				#Kehwan: If each car subroutine gets to return the state after its move, then car #2 will get the state AFTER car #1 moved, and the cars are not moving "in parallel" each turn, but one after the other.
-								#Kehwan: Also, as long as main uncritically accepts the state car() returns, car() can cheat. Maybe we should just have the car()s return a number between 1 and 9 representing how the cars want to accelerate next turn. Main would update the vx,vy of the cars, and do the "driving" and handle events like crashes and cars going off track and cars going through "FINISH" squares.
- $L = checkFinish(@{$STATE{car}}[0]);
+while ($L == 0) {
+  
+  foreach (keys %STATE) {
+    if ($STATE{$_}{mode} eq 'car1') {
+      %STATE=&car1(%STATE);
+    }
+    if ($STATE{$_}{mode} eq 'car1') {
+      %STATE=&car2(%STATE);
+    }
+    
+  }
+  
+  $L = &check_Finish();
+  &check_collision();
+  
 
 
 
@@ -40,11 +60,11 @@ while ($L == 0) {
 
 #-------- Subroutines --------#
 
-sub car {
-  my $pos = shift();
+sub car1 {
+  my %STATE = @_;
+  print "car1\n";
 
-
-  return $pos;
+  return %STATE;
 }
 
 
@@ -59,12 +79,20 @@ sub readin_textfile {
   return @track;
 }
 
-sub checkFinish {
-  my ($x, $y) = @_;
-  $L = 1 if ($x == '1');
+sub set_start {
+  #weist allen autos eine startposition zu
 }
 
+sub check_Finish {
+  #checkt ob ein oder mehrere autos das ziel ereich haben 
+}
 
+sub check_collistion{
+  #schaut ob es collisionen gab, zw. autos, bwz mit streckenrand
+
+
+
+__END__
 =pod
 
 =head1 NAME
@@ -73,17 +101,21 @@ SchoolBenchRacer.pl - old school racing game
 
 =head1 SYNOPSIS
 
-SBR.pl [[-racer I<STRING>] [-track I<STRING>] [-help|?] [-man]]
+SBR.pl [[-racer I<STRING>] [-track I<STRING>] [-modes] [-help|?] [-man]]
 
 =over 4
 
 =item B<-racer>
 
-Names of the racers separated by ":" (in this stage also the names of the subroutines which lead the racer)
+Names of the racers separated by ":" from the mode used (q.v. -mode); each racer:mode pair is separated by "," from the next. S<C<Example: SBR.pl -racer foo:car1,bar:car2>>
 
 =item B<-track>
 
 Location and name of the file containing the racing track; it should be a 2D Matrix where the useabel track is marked with a '1' and the surrounding is marked with a '0'
+
+=item B<-modes>
+
+Lists all modes available to navigate your car through the track.
 
 =item B<-help>
 
@@ -92,4 +124,6 @@ Show synopsis.
 =item B<-man>
 
 Show man page.
+
+=back
 
