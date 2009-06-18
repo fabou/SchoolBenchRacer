325cc13ca02f7162fb0d133fb7a29af833163d11
fabou <fabian_am@msn.com>
2009-06-18 13:38:27 +0200

    RennstreckenGenerator.jar eingebaut; GetOp track obsolet

--- SBR_main.pl
+++ SBR_main.pl
@@ -4,8 +4,8 @@
 
 use strict;
 use warnings;
-use Data::Dumper;
 use Getopt::Long;
+use Data::Dumper;
 use Pod::Usage;
 use vars qw/@RaceTrack %STATE $map_file $L/;
 
@@ -27,7 +27,7 @@ pod2usage(-verbose => 0)
       "modes"   => sub{print "@modes\n";
 		       exit;
       },
-      "track:s" => sub{ $map_file = $_[1] },
+      #"track:s" => sub{ $map_file = $_[1] },
       "help|?"  => sub{pod2usage(-verbose => 1)},
       "man"     => sub{pod2usage(-verbose => 2)}
     );
@@ -38,7 +38,6 @@ pod2usage(-verbose => 0)
 @RaceTrack = readin_textfile();
 
 %STATE = set_start(\@RaceTrack, \%STATE);
-print Dumper(%STATE);
 
 while ($L == 0) {
   
@@ -119,8 +118,16 @@ sub player {                    #subroutine zum haendischen steuern des autos, u
 #------------- Subroutines -------------#
 
 sub readin_textfile {
+  my $status = system("java -jar  ./RennstreckenGenerator.jar");
+  if (($status >>=8) !=0) {
+    die "WARNING: Failed to run the Track Generator!\a\n";
+  }
+
   my @track;
-  open MAP, "<${map_file}" or die "WARNING: can not open the input racing track!\a\n";
+
+
+  
+  open MAP, "< Racetrack.txt" or die "WARNING: can not open the racing track!\a\n";
   while (<MAP>) {
     next if /^\# /;
     push(@track, [split()]);
@@ -182,17 +189,13 @@ SchoolBenchRacer.pl - old school racing game
 
 =head1 SYNOPSIS
 
-SBR.pl [[-racer I<STRING>] [-track I<STRING>] [-modes] [-help|?] [-man]]
+SBR.pl [[-racer I<STRING>] [-modes] [-help|?] [-man]]
 
 =over 4
 
 =item B<-racer>
 
 Names of the racers separated by ":" from the mode used (q.v. -mode); each racer:mode pair is separated by "," from the next. S<C<Example: SBR.pl -racer foo:car1,bar:car2>>
-
-=item B<-track>
-
-Location and name of the file containing the racing track; it should be a 2D Matrix where the useabel track is marked with a '1' and the surrounding is marked with a '0'
 
 =item B<-modes>
 
