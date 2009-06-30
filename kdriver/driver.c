9449159d634530c1954df52d94d4af4960125ffd
Kehwan <kehwan@gmail.com>
2009-06-30 05:09:00 +0200

    changed comments in ToDo, driver.c

--- kdriver/driver.c
+++ kdriver/driver.c
@@ -11,7 +11,6 @@
 #define ifdebug 			if (cmdline.verbose_flag)
 #define rep 			ifdebug fprintf(stderr, "alive, line: %i\n", __LINE__)
 #include "matrixmacros.h"
-#include "representation.h"
 
 struct s_map {
 		int Xsize;
@@ -49,7 +48,6 @@ int MyMove(state_before_move){	/*returns a number between 0 and 9 representing a
 
 int *	Strategy_of_one_who_is_unaware_of_the_existence_of_others(const struct s_map map, const int my_x0, const int my_y0, const int my_vx0, const int my_vy0, const int i_skip_initial_turn)  /*=my_initial_position, my_initial_velocity, do I skip next turn*/){	/*ToDo: shorter name.*/
 	/*returns a sequence of moves that is perfect, assuming no collisions with other cars occur along the way*/
-	/*how to encode map? see representation.h*/
 
 	/*So how does this now proceed? It builds a tree of single player game states*/
 	/*A state is like [a scenery with:] a map with the car and velocity vector drawn into it, and a turn counter standing next to this... and: a red lamp on the wall on the left of the turn map, labeled "Skip Next Turn"; a banner above the map saying "YOU HAVE WON" (or nothing)*/ /*And a
@@ -62,16 +60,9 @@ int *	Strategy_of_one_who_is_unaware_of_the_existence_of_others(const struct s_m
 	/*So, we'll begin at the initial state, and move to one of the states below that, and from there... */
 
 	/* I may want to use one of these
-		a.) A function to determine reliably, for a given state, a minimum (Lbound) for how many turns it will take to go from here to a victory ,
-			(wiki: "admissible heuristic estimate" of the distance to a goal from this state + what is on the turn counter)
-			this + what is on the turn counter = a lower bound for how many turns the best state 'below' this state, will have, on its turn counter
-		This function, for a state, can take into account:
-			the state (am I moving backwards?),
-			the information gathered by the search up to that point
-			"any extra knowledge about the problem"
-	*/
+		a.) A function to determine reliably, for a given state, a minimum (Lbound) for how many turns it will take to go from here to a victory*/
 
-
+	/*A better way might be to use some sort of dynamic programming to calculate the shortest path from the current square, (with the current velocity vector) to the all other squares...*/
 
 }
 
